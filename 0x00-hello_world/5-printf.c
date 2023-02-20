d6d1b7853effe31c4b583e28b661772df866cb8e
Mashia-Abilba <mashia2512@gmail.com>
2023-02-19 18:36:13 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-  * main - main
- *Return 0 (success);
+  * main - Entry point
+ *Return: 0 (success);
  */
 int main(void)
-{	
-printf("with proper grammer, but the outcome is a piece of art,\n")
-return (0);
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
