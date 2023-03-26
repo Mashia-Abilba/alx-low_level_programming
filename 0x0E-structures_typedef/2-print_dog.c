c4aa33e2ef7119d8e6066c55d2ca043a1eebf9fc
Mashia-Abilba <mashia2512@gmail.com>
2023-03-26 11:53:00 -0700

    a dog teaches unconditional love

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,5 +16,5 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-	printf("name: %s\nage: %f\nowner: %s\n", d->name, d->age, d->owner);
+	printf("name: %s\n age: %f\n owner: %s\n", d->name, d->age, d->owner);
 }
